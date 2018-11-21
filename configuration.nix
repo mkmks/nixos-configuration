614a3f3b846bdc2f080362c389130076ee09fa0e
Nikita Frolov <nf@mkmks.org>
2018-11-21 11:41:11 +0100

    move non-autogenerated entries from hardware-configuration

--- configuration.nix
+++ configuration.nix
@@ -15,9 +15,13 @@ in
 
   boot = {
     cleanTmpDir = true;
+    earlyVconsoleSetup = true;    
   };
   
   hardware = {
+    bluetooth.enable = true;
+    cpu.intel.updateMicrocode = true;
+
     opengl = {
       driSupport32Bit = true;
       extraPackages = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl ];  
@@ -28,6 +32,15 @@ in
       package = pkgs.pulseaudioFull;
       support32Bit = true;
     };
+
+    trackpoint = {
+      enable = true;
+      emulateWheel = true;
+      sensitivity = 100;
+      speed = 80;
+    };
+    
+    usbWwan.enable = true;
   };
   
   networking = {
@@ -279,6 +292,16 @@ in
       enableEditActions = true;
     };
 
+    tlp = {
+      enable = lib.mkDefault true;
+      extraConfig = ''
+        START_CHARGE_THRESH_BAT0=75
+        STOP_CHARGE_THRESH_BAT0=80
+        CPU_SCALING_GOVERNOR_ON_BAT=powersave
+        ENERGY_PERF_POLICY_ON_BAT=powersave
+      '';
+    };
+
     tor = {
       enable = false;
       client.enable = true;
@@ -331,7 +354,61 @@ in
   };
 
   systemd = {
+  
+    services = {
+      cpu-throttling = {
+        enable = true;
+        description = "Sets the offset to 3 °C, so the new trip point is 97 °C";
+        documentation = [ "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
+        ];
+        path = [ pkgs.msr-tools ];
+        script = "wrmsr -a 0x1a2 0x3000000";
+        serviceConfig = {
+          Type = "oneshot";
+        };
+        wantedBy = [
+          "timers.target"
+        ];
+      };
 
+      powertop = {
+        description = ''
+          enables powertop's recommended settings on boot
+        '';
+        wantedBy = [ "multi-user.target" ];
+
+        path = with pkgs; [ powertop ];
+
+        environment = {
+          TERM = "dumb";
+        };
+
+        serviceConfig = {
+          Type = "idle";
+          User = "root";
+          ExecStart = ''
+            ${pkgs.powertop}/bin/powertop --auto-tune
+          '';
+        };
+      };
+    };
+
+    timers.cpu-throttling = {
+      enable = true;
+      description = "Set cpu heating limit to 97 °C";
+      documentation = [
+     "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
+      ];
+      timerConfig = {
+        OnActiveSec = 60;
+        OnUnitActiveSec = 60;
+        Unit = "cpu-throttling.service";
+      };
+      wantedBy = [
+        "timers.target"
+      ];
+    };
+    
     user = {
       services = {    
         mbsync = {
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,8 +4,6 @@
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
   boot = {
-    earlyVconsoleSetup = true;
-
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
@@ -57,81 +55,4 @@
 
   nix.maxJobs = lib.mkDefault 8;
 
-  # ThinkPad X1 6G QHD (copied from nixos-hardware)
-
-  hardware = {
-    bluetooth.enable = true;
-    cpu.intel.updateMicrocode = true;
-    trackpoint = {
-      enable = true;
-      emulateWheel = true;
-      sensitivity = 100;
-      speed = 80;
-    };
-    usbWwan.enable = true;
-  };
-
-  systemd.services = {
-    cpu-throttling = {
-      enable = true;
-      description = "Sets the offset to 3 °C, so the new trip point is 97 °C";
-      documentation = [ "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
-      ];
-      path = [ pkgs.msr-tools ];
-      script = "wrmsr -a 0x1a2 0x3000000";
-      serviceConfig = {
-        Type = "oneshot";
-      };
-      wantedBy = [
-        "timers.target"
-      ];
-    };
-
-    powertop = {
-      description = ''
-        enables powertop's recommended settings on boot
-      '';
-      wantedBy = [ "multi-user.target" ];
-
-      path = with pkgs; [ powertop ];
-
-      environment = {
-        TERM = "dumb";
-      };
-
-      serviceConfig = {
-        Type = "idle";
-        User = "root";
-        ExecStart = ''
-          ${pkgs.powertop}/bin/powertop --auto-tune
-        '';
-      };
-    };
-  };
-
-  systemd.timers.cpu-throttling = {
-    enable = true;
-    description = "Set cpu heating limit to 97 °C";
-    documentation = [
-      "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
-    ];
-    timerConfig = {
-      OnActiveSec = 60;
-      OnUnitActiveSec = 60;
-      Unit = "cpu-throttling.service";
-    };
-    wantedBy = [
-      "timers.target"
-    ];
-  };
-
-  services.tlp = {
-    enable = lib.mkDefault true;
-    extraConfig = ''
-      START_CHARGE_THRESH_BAT0=75
-      STOP_CHARGE_THRESH_BAT0=80
-      CPU_SCALING_GOVERNOR_ON_BAT=powersave
-      ENERGY_PERF_POLICY_ON_BAT=powersave
-    '';
-  };
 }
